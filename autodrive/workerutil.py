5b349f55a7855df5df4dd51797632c8224e83cdc
Kevin Zheng <sunday0303@gmail.com>
2017-12-04 05:15:57 -0500

    Add framework for handling multiple controllers

---
+++ autodrive/workerutil.py
@@ -0,0 +1,25 @@
+'''
+Utilities for worker controllers.
+'''
+
+class ControlCommand(object):
+  '''A control command.
+ 
+  cmdtype must be one of:
+    start
+    stop
+    steer
+    speed
+  '''
+  def __init__(self, cmdtype, value=0):
+    assert cmdtype in ('start', 'stop', 'steer', 'speed')
+    assert -10 <= value <= 10
+    self.cmdtype = cmdtype
+    self.value = value
+
+
+def get_img(img_req, img_q):
+  '''Get an image from the master camera process.'''
+  img_req.send(True)
+  return img_q.get()
+
