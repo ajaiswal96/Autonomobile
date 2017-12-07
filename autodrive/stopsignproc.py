4ba26e8446d57f60ab422e1e8b04e81c6bd7b648
Kevin Zheng <sunday0303@gmail.com>
2017-12-07 16:48:58 -0500

    Improve stop sign detection process

--- autodrive/stopsignproc.py
+++ autodrive/stopsignproc.py
@@ -14,6 +14,8 @@ STOPSIGN_PARAMS = 'resources/stopsign_classifier.xml'
 
 SIGN_SIZE_THRESHOLD = 0.1
 
+STOP_SIGN_COUNT = 1
+
 def stopsign_proc(img_req, img_q, cmd_q):
   '''The stop sign worker process.'''
   classifier = cv2.CascadeClassifier(STOPSIGN_PARAMS)
@@ -23,31 +25,34 @@ def stopsign_proc(img_req, img_q, cmd_q):
   while True:
     # get a frame, and save a copy of it for later
     fr = get_img(img_req, img_q)
+    fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
     fr = cv2.resize(fr, (320, 240))
-    w, h = fr.shape[:2]
 
     now = time.time()
 
-    color_fr = fr.copy()
-
     # do the cascade classifier
-    fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-    signs = classifier.detectMultiScale(fr, 1.02, 10)
+    #signs = classifier.detectMultiScale(fr, 1.02, 10)
+    signs = classifier.detectMultiScale(fr, 1.02, 5)
 
     for x, y, w, h in signs:
       sz = (float(w) + float(h)) / 2.0
       dt = px2time(sz)
       suggested_stop_times.append(now + dt)
 
-    if len(suggested_stop_times) > 3 and np.median(suggested_stop_times) < now:
+    if suggested_stop_times:
+      print [t - now for t in suggested_stop_times]
+
+    if len(suggested_stop_times) >= STOP_SIGN_COUNT and np.median(suggested_stop_times) < now:
       cmd_q.put(ControlCommand('start'))
       cmd_q.put(ControlCommand('speed', 0))
       time.sleep(2)
       cmd_q.put(ControlCommand('stop'))
       time.sleep(5)
+      suggested_stop_times = []
 
 def px2time(sign_size):
-  -0.0375 * sign_size + 2.75
+  return -0.06 * sign_size + 3.6
+  #return 55 / sign_size - 0.8
 
 def is_stopsign(fr):
   '''Determine if a frame contains a stop sign'''
